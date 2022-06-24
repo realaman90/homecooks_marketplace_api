c59a0ffb26633564a35e7e8ac0584198d0fffc57
Aman <aman.rawat@outlook.fr>
2022-06-24 12:09:42 +0200

    initial setup

--- route/authRoutes.js
+++ route/authRoutes.js
@@ -4,7 +4,7 @@ const { route } = require('express/lib/router');
 
 const router = express.Router()
 
-const { register, login, logout } = require('../controllers/authController');
+const { register, login, logout } = require('../controllers/auth.controller');
 
 router.post('/register', register);
 router.post('/login', login);
