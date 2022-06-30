64fe30452855d74eaed6f1acdbf0cfe413a91cd9
Aman <aman.rawat@outlook.fr>
2022-06-30 07:15:08 +0200

    minor fixes in supplier model & controller

--- models/Supplier.js
+++ models/Supplier.js
@@ -14,11 +14,29 @@ const SupplierSchema = mongoose.Schema({
         latitude: Number,
         longitude: Number
     },
+    pickupAddress: {
+        street: String,
+        appartment_house: String,
+        city: String,
+        state: String,
+        zipCode: Number,
+        country: String,
+        latitude: Number,
+        longitude: Number
+    },
     businessImages: [String],
     contactInfo: {
         businessPhone: Number,
         businessEmail: String,
     },
+    bankInfo: {
+        accountHolderName: String,
+        accountNumber: Number,
+        routingNumber: Number,
+        venmo: String,
+        zellz: String,
+        cashApp: String
+    }
 
 }, { timestamps: true });
 module.exports = mongoose.model('Supplier', SupplierSchema)
