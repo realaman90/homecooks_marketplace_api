3f03d2cb57fa3bd3678ebec4636634a4bb9b9456
Aman <aman.rawat@outlook.fr>
2022-08-09 00:39:13 +0200

    user auth routes

--- route/customers.routes.js
+++ route/customers.routes.js
@@ -11,8 +11,16 @@ const {
     getAllCustomers,
     getSingleCustomer,
 } = require('../controllers/user.controller');
+const { registerUser, login, resetPasswordOTP, resetPassword } = require('../controllers/auth.controller');
+const { verfyOTP } = require('../controllers/verification.otp');
 
 router.get('/', getAllCustomers);
-router.get('/:id', getSingleCustomer)
+router.get('/:id', getSingleCustomer);
+router.route('/register').post(registerUser);
+router.post('/login', login);
+router.post('/resetPasswordOTP', resetPasswordOTP)
+router.post('/resetPassword', resetPassword)
+router.post('/verify', verfyOTP)
+
 
 module.exports = router;
--- utils/send.OTP.js
+++ utils/send.OTP.js
@@ -1,4 +1,6 @@
 const twilioClient = require('./twilio.client');
+const sgMail = require('./sendgrid.client');
+
 const OTP = require('../models/OTP');
 const sendOTP = async(userFromDB, reason) => {
     const fourDigitOTP = Math.floor(1000 + Math.random() * 9000);
@@ -9,11 +11,35 @@ const sendOTP = async(userFromDB, reason) => {
         otp: fourDigitOTP,
         reason
     });
-    const response = await twilioClient.messages.create({
-        from: process.env.TWILIO_PHONE_NUMBER,
-        to: "+" + userFromDB.phone,
-        body: message,
-    });
+    //phone
+    if (userFromDB.role != 'user') {
+        const response = await twilioClient.messages.create({
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to: "+" + userFromDB.phone,
+            body: message,
+        });
+    }
 
+    //email
+
+    const msg = {
+        to: `${userFromDB.email}`, // Change to your recipient
+        from: `${process.env.SGSENDER}`, // Change to your verified sender
+
+        templateId: process.env.OTPTEMPLATEID,
+        dynamicTemplateData: {
+            subject: `${userFromDB.fullName} your verification code`,
+            name: `${userFromDB.fullName}`,
+            otp: `${fourDigitOTP}`,
+        },
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 }
 module.exports = sendOTP;
