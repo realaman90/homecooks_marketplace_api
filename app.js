9751ee9040a424e180f318f0b04f83546afd9418
utkarsh-dev <utkarsh17ife@gmail.com>
2022-09-04 14:52:42 +0530

    fix: rem rate lim

--- app.js
+++ app.js
@@ -48,10 +48,10 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust-proxy', 1);
-app.use(rateLimiter({
-    windowMs: 1800 * 60 * 1000,
-    max: 9000
-}));
+// app.use(rateLimiter({
+//     windowMs: 1800 * 60 * 1000,
+//     max: 9000
+// }));
 app.use(helmet());
 app.use(cors());
 app.use(mongoSanatize());
